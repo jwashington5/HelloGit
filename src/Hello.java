4c2fa414c4414fdc1e565b8259b140f1acadf23c
Jakobi <Jakobi@10.64.58.13>
2023-08-26 15:49:47 -0500

    Added major and comments

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,17 @@
+/**
+ * Author: Jakobi Washington 
+ * Date: 2023/08/26
+ * 
+ * Prints name and major 
+ * 
+ * @author Jakobi
+ *
+ */
 
 public class Hello {
 	
 	public static void main(String args[]) {
 		System.out.println("Jakobi Washington");
+		System.out.println("Major: Computer Science");
 	}
 }
